dd9f01464146042737d0193b75e0e929f9ca546d
AlexCrownshaw <alex.crownshaw12@gmail.com>
2022-09-30 23:50:39 +0100

    Add state_derivative and computation function

--- Simulation/Dynamics_Model.py
+++ Simulation/Dynamics_Model.py
@@ -3,14 +3,15 @@ import json
 import numpy as np
 
 from typing import Tuple
-from Simulation.Time_State import TimeState
+from Simulation.Time_State import TimeState, StateDerivative
 
 
 class DynamicsModel:
 
     g = 9.81  # m/s^2
 
-    def __init__(self, structural_json_path, flight_path_json_path):
+    def __init__(self, t_delta, structural_json_path, flight_path_json_path):
+        self.t_delta = t_delta
 
         # Load simulation config properties
         self.properties, self.dimensions = self.load_structural_json(structural_json_path)
@@ -35,14 +36,14 @@ class DynamicsModel:
             json_object = json.load(json_file)
             return json_object["Boundary_Conditions"], json_object["Maneuvers"]
 
-    def compute_moments(self, motor_thrusts: np.array) -> np.array:
-        L = np.sum(motor_thrusts * np.array([self.dimensions["d_y"], -self.dimensions["d_y"], -self.dimensions["d_y"],
-                                             self.dimensions["d_y"]]))
-        M = np.sum(motor_thrusts * np.array([self.dimensions["d_x"], -self.dimensions["d_x"], self.dimensions["d_x"],
-                                             -self.dimensions["d_x"]]))
-        N = np.sum(np.array([0, 0, 0]))
+    def compute_moments(self, motor_thrusts: np.array) -> Tuple[float, float, float]:
+        L = float(np.sum(motor_thrusts * np.array([self.dimensions["d_y"], -self.dimensions["d_y"],
+                                                   -self.dimensions["d_y"], self.dimensions["d_y"]])))
+        M = float(np.sum(motor_thrusts * np.array([self.dimensions["d_x"], -self.dimensions["d_x"],
+                                                   self.dimensions["d_x"], -self.dimensions["d_x"]])))
+        N = float(np.sum(np.array([0, 0, 0])))
 
-        return np.array([L, M, N])
+        return L, M, N
 
     @staticmethod
     def thrust_vector_body(motor_thrusts) -> np.array:
@@ -52,20 +53,50 @@ class DynamicsModel:
         return np.array([-self.g * np.sin(theta), self.g * np.sin(phi) * np.cos(theta),
                          self.g * np.cos(phi) * np.cos(theta)]).reshape(3, 1)
 
-    def compute_state_derivative(self, prev: TimeState, motor_thrusts: np.array) -> \
-            Tuple[np.array, np.array, np.array, np.array]:
+    def compute_state_derivative(self, X: TimeState, U) -> StateDerivative:
 
-        force_vector = self.thrust_vector_body(motor_thrusts) + self.gravity_vector_body(prev.theta, prev.phi)
-        V_dot = force_vector - np.array([prev.q * prev.w - prev.r * prev.v, prev.r * prev.u - prev.p * prev.w,
-                                         prev.p * prev.v - prev.q * prev.u]).reshape(3, 1)
+        # Pre-calculate trigonometric terms
+        cos_phi = np.cos(X.phi)
+        sin_phi = np.sin(X.phi)
+        cos_theta = np.cos(X.theta)
+        sin_theta = np.sin(X.theta)
+        cos_psi = np.cos(X.psi)
+        sin_psi = np.sin(X.psi)
 
-        rotational_body = np.array()
-        linear_inertial = np.array()
-        rotational_inertial = np.array()
+        # Forces sum up in the body z-axis
+        F_z = -np.sum(U.thrusts)
 
-        return V_dot, rotational_body, linear_inertial, rotational_inertial
+        # Compute Moments
+        L, M, N = self.compute_moments(U.thrusts)
 
-    def rk4(self) -> TimeStep:
+        # Define state derivative object
+        sd = StateDerivative()
+
+        # Compute linear body acceleration
+        sd.u_dot = -self.g * sin_theta + X.r * X.v - X.q * X.w
+        sd.v_dot = self.g * sin_phi * cos_theta - X.r * X.u + X.p * X.w
+        sd.w_dot = 1 / self.properties["mass"] * F_z + self.g * cos_phi * cos_theta + X.q * X.u - X.p * X.v
+
+        # Compute rotational body acceleration
+        sd.p_dot = 1 / self.properties["I_xx"] * (L + (self.properties["I_yy"] - self.properties["I_zz"]) * X.q * X.r)
+        sd.q_dot = 1 / self.properties["I_yy"] * (M + (self.properties["I_zz"] - self.properties["I_xx"]) * X.p * X.r)
+        sd.r_dot = 1 / self.properties["I_zz"] * (N + (self.properties["I_xx"] - self.properties["I_yy"]) * X.p * X.q)
+
+        # Compute linear inertial acceleration
+        sd.x_dot = cos_theta * cos_psi * X.u + (-cos_phi * sin_psi + sin_phi * sin_theta * cos_psi) * X.v + \
+                   (sin_phi * sin_psi + cos_phi * sin_theta * cos_psi) * X.w
+        sd.y_dot = cos_theta * sin_psi * X.u + (cos_phi * cos_psi + sin_phi * sin_theta * sin_psi) * X.v + \
+                   (-sin_phi * cos_psi + cos_phi * sin_theta * sin_psi) * X.w
+        sd.z_dot = -1 * (-sin_theta * X.u + sin_phi * cos_theta * X.v + cos_phi * cos_theta * X.w)
+
+        # Compute rotational inertial acceleration
+        sd.psi_dot = (X.q * sin_phi + X.r * cos_phi) / cos_theta
+        sd.theta_dot = X.q * cos_phi - X.r * sin_phi
+        sd.phi_dot = X.p + (X.q * sin_phi + X.r * cos_phi) * sin_theta / cos_theta
+
+        return sd
+
+    def rk4(self) -> TimeState:
         pass
 
     @staticmethod
@@ -84,7 +115,3 @@ class DynamicsModel:
             r = r.transpose()
 
         return r
-
-    def run_simulation(self) -> None:
-        for t in range(0, self.t_duration / self.delta_t, self.delta_t):
-             u = pid
