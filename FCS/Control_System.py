7e354eb5d54aa311acdb4967935d2652a2ae38cc
AlexCrownshaw <alex.crownshaw12@gmail.com>
2022-10-01 23:47:36 +0100

    working dynamics_model add data processing

--- FCS/Control_System.py
+++ FCS/Control_System.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from FCS.PID import PID
+from Simulation.Time_State import TimeState
 
 
 class ControlSystem:
 
     motor_limit = 1000
 
-    def __init__(self, gain_x, gain_y, gain_z, gain_yaw, gain_pitch, gain_roll) -> None:
+    def __init__(self, maneuvers, gain_x, gain_y, gain_z, gain_yaw, gain_pitch, gain_roll) -> None:
 
         # Translation PID objects
         self.pid_x = PID(gain_x[0], gain_x[1], gain_x[2], self.motor_limit)
@@ -20,8 +21,15 @@ class ControlSystem:
         self.pid_yaw = PID(gain_roll[0], gain_roll[1], gain_roll[2], self.motor_limit)
 
     @staticmethod
-    def run_control_loop() -> np.array:
+    def run_control_loop(X: TimeState) -> np.array:
+        motor_thrusts = np.array([0.1, 0.1, 0.1, 0.1])
 
-        motor_thrusts = np.array([])
+        if X.x < 2:
+            motor_thrusts[0], motor_thrusts[2] = 0.1001, 0.1001
+            motor_thrusts[1], motor_thrusts[3] = 0.1, 0.1
+        else:
+            motor_thrusts[0], motor_thrusts[2] = 0.1, 0.1
+            motor_thrusts[1], motor_thrusts[3] = 0.1001, 0.1001
 
-        return motor_thrusts
+        return motor_thrusts * 10
+
--- Main.py
+++ Main.py
@@ -1,11 +1,16 @@
+import json
+
 import numpy as np
 
+from typing import Tuple
 from FCS.Control_System import ControlSystem
 from Simulation.Dynamics_Model import DynamicsModel
+from Simulation.Time_State import TimeState
+from Data_Processing.Data_Processing import DataProcessing
 
 # Simulation time variables
-t_duration = 100
-t_delta = 0.001
+t_duration = 30
+t_delta = 0.02
 
 # PID Gain Values [Kp, Ki, Kd]
 gain_x = [1, 1, 1]
@@ -22,21 +27,49 @@ FLIGHT_PATH_JSON_PATH = r"Config_JSON/Flight_Plan/Flight_Path.json"
 
 
 def main():
+    # Load Config properties
+    properties, dimensions = load_structural_json(STRUCTURAL_JSON_PATH)
+    IC, maneuvers = load_flight_path_json(FLIGHT_PATH_JSON_PATH)
 
     # Instantiate control system object
-    control = ControlSystem(gain_x, gain_y, gain_z, gain_yaw, gain_pitch, gain_roll)
+    control = ControlSystem(maneuvers, gain_x, gain_y, gain_z, gain_yaw, gain_pitch, gain_roll)
 
     # Instantiate dynamics Simulation Object
-    dynamics = DynamicsModel(t_delta, STRUCTURAL_JSON_PATH, FLIGHT_PATH_JSON_PATH)
+    dynamics = DynamicsModel(properties, dimensions)
 
-    # x = Initial conditions
+    # Set up initial conditions
+    state_vector = np.array([0, 0, 0, 0, 0, 0, IC["x"], IC["y"], IC["z"], 0, 0, 0])
+    X = TimeState(state_vector)
 
-    t_steps = np.arange(0, t_duration / t_delta + t_delta, t_delta)
+    sim_data = DataProcessing()
 
-    for t in t_steps:
+    for t in np.arange(0, t_duration + t_delta, t_delta):
+        t = round(t, 4)
 
-        u = control.run_control_loop()
-        x = dynamics.rk4(x, u)
+        U = control.run_control_loop(X)
+        X = dynamics.rk4(X, U, t_delta)
+
+        sim_data.append_time_instance(t, X, U)
+
+        print("Calculation Complete for t = {}s".format(t))
+
+    # sim_data.plot_position()
+    # sim_data.plot_attitude()
+
+    sim_data.plot_inertial()
+    sim_data.plot_3d()
+
+
+def load_structural_json(json_path: str) -> Tuple[dict, dict]:
+    with open(json_path) as json_file:
+        json_object = json.load(json_file)
+        return json_object["properties"], json_object["dimensions"]
+
+
+def load_flight_path_json(json_path: str) -> Tuple[dict, dict]:
+    with open(json_path) as json_file:
+        json_object = json.load(json_file)
+        return json_object["Initial_Conditions"], json_object["Maneuvers"]
 
 
 if __name__ == "__main__":
